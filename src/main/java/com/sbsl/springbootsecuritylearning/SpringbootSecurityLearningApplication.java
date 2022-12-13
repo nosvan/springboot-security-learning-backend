990d90de0cdde2da2ed82b971294ef511c126af9
nos <>
2022-12-13 17:46:06 -0500

    initial push

---
+++ src/main/java/com/sbsl/springbootsecuritylearning/SpringbootSecurityLearningApplication.java
@@ -0,0 +1,13 @@
+package com.sbsl.springbootsecuritylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbsl/springbootsecuritylearning/SpringbootSecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbsl.springbootsecuritylearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
